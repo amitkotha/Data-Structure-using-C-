20df3a8652b1b5631ca6bd93e698378b13dbacba
amitkotha <amitkotha1989@gmail.com>
2021-05-06 02:08:03 +0530

    Linked List

---
+++ Linked List/Linked List/Linked List/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinkedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Append("First Node");
+            linkedList.Append("Second Node");
+            linkedList.PrintList();
+            linkedList.Prepend("Prepend Node");
+            linkedList.PrintList();
+            linkedList.Insert(2, "Insert Node");
+            linkedList.PrintList();
+            linkedList.Remove(2);
+            linkedList.PrintList();
+            Console.ReadLine();
+        }
+    }
+}
