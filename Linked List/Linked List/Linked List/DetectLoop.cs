d6dbc6c1d7999601880a406aa3abef22d48fb796
amitkotha <amitkotha1989@gmail.com>
2021-05-07 02:36:52 +0530

    Added Detect loop

---
+++ Linked List/Linked List/Linked List/DetectLoop.cs
@@ -0,0 +1,50 @@
+﻿using LinkedLists;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_List
+{
+  public  class DetectLoop
+    {
+
+        public int LoopCount(LinkedList linkedList)
+        {
+
+            if (linkedList == null)
+            {
+                return 0;
+            }
+
+            if (linkedList.Head == linkedList.Head.Next)
+            {
+                return 1;
+            }
+            int count = 0;
+            Node pointer = linkedList.Head;
+            Node pointer1 = linkedList.Head.Next;
+            bool hasLoop = false;
+            while (pointer != pointer1 && pointer1 != null && pointer1.Next != null)
+            {
+                pointer = pointer.Next;
+                pointer1 = pointer1.Next.Next;
+
+                if (pointer == pointer1)
+                {
+                    hasLoop = true;
+                    break;
+
+                }
+            }
+            if (!hasLoop) return 0;
+
+            do
+            {
+                pointer = pointer.Next;
+                count++;
+            } while (pointer != pointer1);
+
+            return count;
+        }
+    }
+}
--- Linked List/Linked List/Linked List/Program.cs
+++ Linked List/Linked List/Linked List/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Linked_List;
+using System;
 
 namespace LinkedLists
 {
@@ -6,16 +7,25 @@ namespace LinkedLists
     {
         static void Main(string[] args)
         {
+            Node n = new Node("Loop Node");
             LinkedList linkedList = new LinkedList();
+
+            Console.WriteLine("Adding the First Node");
             linkedList.Append("First Node");
+
+            Console.WriteLine("Adding the Second Node");
             linkedList.Append("Second Node");
+
+            Node find = linkedList.Find("Second Node");
+            
+            linkedList.Append("Third Node");
             linkedList.PrintList();
-            linkedList.Prepend("Prepend Node");
-            linkedList.PrintList();
-            linkedList.Insert(2, "Insert Node");
-            linkedList.PrintList();
-            linkedList.Remove(2);
-            linkedList.PrintList();
+
+            linkedList.Tail.Next = find;
+
+            DetectLoop detectLoop = new DetectLoop();
+            Console.WriteLine(detectLoop.LoopCount(linkedList));
+            
             Console.ReadLine();
         }
     }
